cbab5cf1d208ae3d57b4aad6d054e3c1706aeb2a
Heeyoung Song <hsong8179@gmail.com>
2020-12-18 10:44:25 -0500

    fixed bug

--- intermediate/*whatIsInAName.js
+++ intermediate/*whatIsInAName.js
@@ -4,18 +4,25 @@
 // For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), 
 // because it contains the name and its value, that was passed on as the second argument.
 function whatIsInAName(collection, source) {
-    const srcKeys = Object.keys(source)
+    let srcKeys = Object.keys(source)
     return collection.filter(object=>{
         for (let i = 0; i < srcKeys.length; i++) {
            if(!object.hasOwnProperty(srcKeys[i]) || object[srcKeys[i]] !== source[srcKeys[i]]){
-               return false
-           }else{
-               return true
-           } 
-            
+               return false;
+           }
         }
+        return true;
     })
+}
+           
+           
    
-  }
 
-  console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }))
+  console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 }))
+
+  /*
+We filter through the array using .filter().
+Using a for loop we loop through each item in the object.
+We use a if statement to check if the object in the collection doesn’t have the key and the property value doesn’t match the value in source.
+We return false if the above if statement is correct. Otherwise, we return true;
+  */
