9c3abfee195d236ae7de00c94f50aac63574feda
Heeyoung Song <hsong8179@gmail.com>
2020-12-22 10:41:38 -0500

    added other solution

--- intermediate/5-spinalTapCase.js
+++ intermediate/5-spinalTapCase.js
@@ -20,3 +20,18 @@ function spinalCase(str) {
   }
 
   console.log(spinalCase("thisIsSpinalTap"))
+
+  //============================================
+
+  function spinalCase(str) {
+    // Replace low-upper case to low-space-uppercase
+    str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+    // Split on whitespace and underscores and join with dash
+    return str
+      .toLowerCase()
+      .split(/(?:_| )+/)
+      .join("-");
+  }
+  
+  // test here
+  spinalCase("This Is Spinal Tap");
