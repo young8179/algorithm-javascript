a95dcda5d5dcbfe73dcfdf157df3710373e31e4a
Heeyoung Song <hsong8179@gmail.com>
2020-12-23 10:21:34 -0500

    cleaned code

--- intermediate/9-missingLetter.js
+++ intermediate/9-missingLetter.js
@@ -3,9 +3,7 @@ function fearNotLetter(str) {
     let strLength = str.length
     const strFirstIndex = alpha.findIndex(element => element == str.charAt(0))
     let count = 0;
-    console.log(strFirstIndex)
     for (let i = strFirstIndex; i <= strFirstIndex + strLength; i++) {
-        console.log([str.charAt(count), alpha[i]])
         if(str.charAt(count) !== alpha[i]){
             result = alpha[i]
             break      
