f875d09ff01c13b2d4920d840f185473c0d90a42
Heeyoung Song <hsong8179@gmail.com>
2020-12-27 15:44:00 -0500

    added exercise

---
+++ intermediate/11-convertHtml.js
@@ -0,0 +1,11 @@
+/*
+Intermediate Algorithm Scripting: Convert HTML Entities
+Convert the characters &, <, >, " (double quote), 
+and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+    return str;
+  }
+  
+  convertHTML("Dolce & Gabbana");
