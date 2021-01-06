57093cba4e6918ea758dece6021412b97ead841f
Heeyoung Song <hsong8179@gmail.com>
2021-01-06 11:43:04 -0500

    added solution

--- intermediate/16-***steamRollArray.js
+++ intermediate/16-***steamRollArray.js
@@ -4,16 +4,16 @@ Flatten a nested array. You must account for varying levels of nesting.
 */
 
 function steamrollArray(arr) {
-    var flattenedArray = [];
+    let flattenedArray = [];
   
     // Create function that adds an element if it is not an array.
     // If it is an array, then loops through it and uses recursion on that array.
     // made another func. it will keep looping until array is gone 
-    var flatten = function(arg) {
+    const flatten = function(arg) {
       if (!Array.isArray(arg)) {
         flattenedArray.push(arg);
       } else {
-        for (var a in arg) {
+        for (let a in arg) {
           flatten(arg[a]);
         }
       }
