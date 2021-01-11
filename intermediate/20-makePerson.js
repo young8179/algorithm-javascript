d97ccb717e66562200f14672e85752846054cdd1
Heeyoung Song <hsong8179@gmail.com>
2021-01-11 10:55:29 -0500

    added other solution

--- intermediate/20-makePerson.js
+++ intermediate/20-makePerson.js
@@ -62,3 +62,37 @@ let Person = function(firstAndLast) {
   let bob = new Person('Bob Ross');
   bob.setFullName("Haskell Curry")
   console.log(bob.getFullName())
+
+
+  //! other solution
+
+  var Person = function(firstAndLast) {
+    var fullName = firstAndLast;
+  
+    this.getFirstName = function() {
+      return fullName.split(" ")[0];
+    };
+  
+    this.getLastName = function() {
+      return fullName.split(" ")[1];
+    };
+  
+    this.getFullName = function() {
+      return fullName;
+    };
+  
+    this.setFirstName = function(name) {
+      fullName = name + " " + fullName.split(" ")[1];
+    };
+  
+    this.setLastName = function(name) {
+      fullName = fullName.split(" ")[0] + " " + name;
+    };
+  
+    this.setFullName = function(name) {
+      fullName = name;
+    };
+  };
+  
+  var bob = new Person("Bob Ross");
+  bob.getFullName();
