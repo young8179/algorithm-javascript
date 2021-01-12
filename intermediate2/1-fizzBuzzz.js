1b52518d7cd93d4ad92fa2500e65de302e20b2e1
Heeyoung Song <hsong8179@gmail.com>
2021-01-12 13:09:37 -0500

    added exercise

---
+++ intermediate2/1-fizzBuzzz.js
@@ -0,0 +1,23 @@
+/*
+Write a program that prints the numbers from 1 to 100. But for multiples of three print 
+“Fizz” instead of the number and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”.
+*/
+
+/*
+For every number that is divisible by 3 and 5, console log "FizzBuzz".
+For every number that is divisible by only 3 and not 5, console log "Fizz".
+For every number that is divisible by only 5 and not 3, console .log "Buzz".
+Here is a partial example with numbers from 12 to 20:
+...
+"Fizz"
+13
+14
+"FizzBuzz"
+16
+17
+"Fizz"
+19
+"Buzz"
+...
+*/
