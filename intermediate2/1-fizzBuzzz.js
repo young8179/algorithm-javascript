79ce2b6eff2d4b34ae11cbb41ed94fe4acdfbcd8
Heeyoung Song <hsong8179@gmail.com>
2021-01-14 10:38:53 -0500

    added exercise

--- intermediate2/1-fizzBuzzz.js
+++ intermediate2/1-fizzBuzzz.js
@@ -21,3 +21,21 @@ Here is a partial example with numbers from 12 to 20:
 "Buzz"
 ...
 */
+
+function fizzbuzz1(){
+    
+    for (let i = 1; i <= 100; i++) {
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz")
+        }else if(i % 3 === 0){
+            console.log("Fizz")
+        }else if(i % 5 === 0){
+            console.log("Buzz")
+        }else{
+            console.log(i)
+        }
+        
+    }
+}
+
+console.log(fizzbuzz1())
