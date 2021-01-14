2bf6e7cc07b51cc1b439db8dc4a7bab3b6c0acc1
Heeyoung Song <hsong8179@gmail.com>
2021-01-14 11:24:25 -0500

    arrange

--- exercise2/16-chunckyMonkey.js
+++ exercise2/16-chunckyMonkey.js
@@ -1,5 +1,6 @@
 // Basic Algorithm Scripting: Chunky Monkey
-// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+// Write a function that splits an array (first argument) into groups the length of size (second argument) 
+// and returns them as a two-dimensional array.
 
 function chunkArrayInGroups(arr, size) {
   var newArr = [];
--- exercise2/3-factorializeNum.js
+++ exercise2/3-factorializeNum.js
@@ -2,9 +2,10 @@ function factorialize(num) {
     let factorNum = 1
     for (let i = 1; i <= num; i++) {
         factorNum = factorNum * i
+        console.log(factorNum)
        
     }
     return factorNum;
   }
 
-  console.log(factorialize(10))
+  console.log(factorialize(5))
