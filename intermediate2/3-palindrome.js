30c12ca3e1e783cc7c1d5dbe357cdc061465bd32
Heeyoung Song <hsong8179@gmail.com>
2021-01-21 10:20:03 -0500

    added solution

--- intermediate2/3-palindrome.js
+++ intermediate2/3-palindrome.js
@@ -1,7 +1,15 @@
 function palindrome(str) {
-    return true;
+    return (
+      str.replace(/[\W_]/g, "").toLowerCase() ===
+      str
+        .replace(/[\W_]/g, "")
+        .toLowerCase()
+        .split("")
+        .reverse()
+        .join("")
+    );
   }
   
   
   
-  palindrome("eye");
+  console.log(palindrome("_eye"))
