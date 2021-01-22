7097a2c5e7b72e2d5fa43ae040e0cadc8b29c953
Heeyoung Song <hsong8179@gmail.com>
2021-01-22 09:45:17 -0500

    added exercise

---
+++ intermediate2/4-knapsack.js
@@ -0,0 +1,10 @@
+/*
+get a maximum value of the items you put into the bag 
+whilst ensuring that you don't exceed the maximum weight.
+*/
+
+const items = [
+    {id: "a", val: 10, w: 3},
+    {id: "b", val: 6, w: 8},
+    {id: "c", val: 3, w: 3}
+]
