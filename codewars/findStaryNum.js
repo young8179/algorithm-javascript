f5a5c2393c9578e6811c9dddaf2c2d0580ce0a42
Heeyoung Song <hsong8179@gmail.com>
2021-02-02 10:42:38 -0500

    added solution

--- codewars/findStaryNum.js
+++ codewars/findStaryNum.js
@@ -7,5 +7,36 @@ The input array will always be valid! (odd-length >= 3)
  */
 
 function stray(numbers) {
-    return 0;
+    let standard = numbers[0]
+    let single = []
+    let multi = []
+    for (let i = 1; i < numbers.length; i++) {
+        if(standard === numbers[i]){
+            multi.push(numbers[i])
+        }else{
+            single.push(numbers[i])
+        }
+    }
+    if(single.length === 1){
+        return Number(single.join(""))
+    }else if(multi.length === 0 ){
+        return numbers[0]
+    }   
+  }
+
+  console.log(stray([17, 17, 3, 17, 17, 17, 17]))
+
+  //other solution
+  function stray1(numbers) {
+    const num = numbers.sort((a, b)=>{
+        return a - b
+    });
+    return (num[0] === num[1]) ? num[num.length-1] : num[0];
+  }
+  console.log(stray1([17, 17, 3, 17, 17, 17, 17]))
+
+
+
+  function stray2(numbers) {
+    return numbers.reduce((x, y) => x ^ y);
   }
