52e84a46d3c8bc08ff341c0a6d22a48ae79fc384
Heeyoung Song <hsong8179@gmail.com>
2021-02-03 10:39:38 -0500

    added exercise

---
+++ codewars/money.js
@@ -0,0 +1,18 @@
+/*
+  Let P be the Principal = 1000.00      
+  Let I be the Interest Rate = 0.05      
+  Let T be the Tax Rate = 0.18      
+  Let D be the Desired Sum = 1100.00
+
+
+After 1st Year -->
+  P = 1041.00
+After 2nd Year -->
+  P = 1083.86
+After 3rd Year -->
+  P = 1128.30
+*/
+
+function calculateYears(principal, interest, tax, desired) {
+    // your code
+}
