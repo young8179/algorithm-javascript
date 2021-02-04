49b005875459ea451d5624d3d2cf8f494d2ea3c2
Heeyoung Song <hsong8179@gmail.com>
2021-02-04 10:11:46 -0500

    added other solution

--- codewars/money.js
+++ codewars/money.js
@@ -34,3 +34,17 @@ function calculateYears(principal, interest, tax, desired) {
 }
 
 console.log(calculateYears(1000, 0.05, 0.18, 1100)) ;
+
+
+//other solution===============================================
+
+
+function calculateYears(principal, interest, tax, desired) {
+  // your code
+  var years = 0;
+  while(principal < desired){
+    principal += (principal * interest) * (1 - tax);
+    years++;
+  }
+  return years;
+}
