f21383ece7d368739b9de3620221e3014d31a9c0
Heeyoung Song <hsong8179@gmail.com>
2021-02-08 09:32:47 -0500

    added exercise and solution

---
+++ codewars/breakCamelCase.js
@@ -0,0 +1,19 @@
+/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+*/
+
+function solution(string) {
+    let result = ""
+    for (let i = 0; i < string.length; i++) {
+        if(string[i] === string[i].toUpperCase()){
+            result += " " + string[i]
+        }else{
+            result += string[i]
+
+        }
+        
+    }
+    return result
+}
+
+console.log(solution("camelCasing"))
