7a69db2355e426d34b848c59e2a9615f996c4e70
Heeyoung Song <hsong8179@gmail.com>
2021-02-10 10:36:27 -0500

    added exercise and solution

--- codewars/extractDomainName.js
+++ codewars/extractDomainName.js
@@ -3,34 +3,20 @@ Write a function that when given a URL as a string,
 parses out just the domain name and returns it as a string. For example:
 */
 
-function domainName(url){
-    let result = url
-    if(url.startsWith("https://www.")){
-       return url.replace(("https://www.", ""))
-    }
-     if(url.startsWith("https://")){
-       result =  url.replace(("https://", ""))
-    }
-    if(url.startsWith("http://www.")){
-       result =  url.replace(("http://www.", ""))
-    }
-    if(url.startsWith("http://")){
-       result =  url.replace("http://", "")
-    }
 
-    // if(result.endWith(".com")){
-    //     result.replace(".com", "")
-    // }
-    
-
-    
-  }
 //solution
   function domainName(url){
    let sourceString = url.replace('http://','').replace('https://','').replace('www.','').split(/[/?#]/)[0];
     let domain = sourceString.split(".")[0];
     return domain;
 }
+
+function domainName1(url){
+   url = url.replace("https://", '');
+   url = url.replace("http://", '');
+   url = url.replace("www.", '');
+   return url.split('.')[0];
+ };
 
   console.log(domainName("https://www.cnet.com"))
 
---
+++ codewars/splitString.js
@@ -0,0 +1,21 @@
+/*
+Complete the solution so that it splits the string into pairs of two characters. 
+If the string contains an odd number of characters 
+then it should replace the missing second character of the final pair with an underscore ('_').
+*/
+
+function solution(str){
+   const strArray = str.split("")
+   let temp = []
+   let result = []
+   while(strArray.length >= 2){
+       let letter = strArray.splice(0,2)
+       result.push(letter.join(""))
+    }
+    if(strArray.length == 1 ){
+        result.push(strArray[0] + "_")
+    }
+    return result;
+}
+
+console.log(solution("abcdef"))
