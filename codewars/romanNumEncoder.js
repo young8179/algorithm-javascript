0c3026577d1611f5a6af3904d244a10b9c8f2ddc
Heeyoung Song <hsong8179@gmail.com>
2021-02-11 11:10:21 -0500

    added solution

--- codewars/romanNumEncoder.js
+++ codewars/romanNumEncoder.js
@@ -71,3 +71,17 @@ function solution(number){
   }
 
   console.log(solution(3000))
+
+  //! solution
+  function solution1(number){
+    var map = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},
+         output = '';
+   
+     for (var i in map ) {
+       while ( number >= map[i] ) {
+         output += i;
+         number -= map[i];
+       }
+     }
+     return output;
+   }
